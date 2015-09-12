1dfc01fad1b9e62f5d40590fdf285af7e2bb8d78
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-09-12 20:24:35 +0200

    Functional

--- bricks.py
+++ bricks.py
@@ -36,11 +36,17 @@ def pixels2sprite(pixels):
 
 class Ball(object):
 
-    def __init__(self, x0, y0, r, vx, vy, color=(1, 1, 1, 1)):
+    def __init__(self, xcen, ycen, r, vx, vy, color=(1, 1, 1, 1)):
 
-        self.x0 = x0
-        self.y0 = y0
+        self.xcen = xcen
+        self.ycen = ycen
+        self.xcen_prev = None
+        self.xcen_prev = None
+
+        self.side_bounce = True
+
         self.r = r
+
         self.vx = vx
         self.vy = vy
 
@@ -49,65 +55,107 @@ class Ball(object):
         pixels = np.array(color) * R.astype(float)[:, :, None] * 255
         self.sprite = pixels2sprite(pixels)
 
+    @property
+    def xmin(self):
+        return self.xcen - self.r
+
+    @property
+    def xmax(self):
+        return self.xcen + self.r
+
+    @property
+    def ymin(self):
+        return self.ycen - self.r
+
+    @property
+    def ymax(self):
+        return self.ycen + self.r
+
     def blit(self):
-        self.sprite.blit(self.x0 - self.r, self.y0 - self.r)
+        self.sprite.blit(self.xmin, self.ymin)
 
     def move(self, dt):
 
-        self.x0 += self.vx * dt
-        self.y0 += self.vy * dt
+        self.xcen_prev = self.xcen
+        self.ycen_prev = self.ycen
 
-        if self.x0 < self.r:
+        self.xcen += self.vx * dt
+        self.ycen += self.vy * dt
+
+        c1 = self.collide_vertical(0)
+        c2 = self.collide_vertical(WINDOW_WIDTH)
+        c3 = self.collide_horizontal(WINDOW_HEIGHT)
+
+    def collide_vertical(self, x, y_range=None, side_bounce=False):
+        if abs(self.xcen - x) < self.r and (y_range is None or y_range[0] < self.ycen < y_range[1]):
             self.vx = -self.vx
-            self.x0 = self.r
-        if self.x0 > WINDOW_WIDTH - self.r:
-            self.vx = -self.vx
-            self.x0 = WINDOW_WIDTH - self.r
-        if self.y0 > WINDOW_HEIGHT - self.r:
+            if self.xcen_prev > x:
+                self.xcen = x + self.r
+            else:
+                self.xcen = x - self.r
+            return True
+        else:
+            return False
+
+    def collide_horizontal(self, y, x_range=None, side_bounce=False):
+        print(self.ycen, self.ymax, y)
+        if abs(self.ycen - y) < self.r and (x_range is None or x_range[0] < self.xcen < x_range[1]):
+            print(self.vy)
             self.vy = -self.vy
-            self.y0 = WINDOW_HEIGHT - self.r
+            if self.ycen_prev > y:
+                self.ycen = y + self.r
+            else:
+                self.ycen = y - self.r
+            if side_bounce:
+                frac = (self.xcen - x_range[0]) / (x_range[1] - x_range[0]) - 0.5
+                self.vx = self.vy * frac
+            return True
+        else:
+            return False
 
     def collides(self, rect):
 
-        if self.x0 > rect.x0 and self.x0 < rect.x0 + rect.dx:
-            if self.y0 < rect.y0 + rect.dy + self.r and self.y0 > rect.y0 - self.r:
-                return True
+        c1 = self.collide_horizontal(rect.ymin, x_range=[rect.xmin, rect.xmax], side_bounce=rect.side_bounce)
+        c2 = self.collide_horizontal(rect.ymax, x_range=[rect.xmin, rect.xmax], side_bounce=rect.side_bounce)
+        c3 = self.collide_vertical(rect.xmin, y_range=[rect.ymin, rect.ymax], side_bounce=rect.side_bounce)
+        c4 = self.collide_vertical(rect.xmax, y_range=[rect.ymin, rect.ymax], side_bounce=rect.side_bounce)
 
-        if self.y0 > rect.y0 and self.y0 < rect.y0 + rect.dy:
-            if self.x0 < rect.x0 + rect.dx + self.r and self.x0 > rect.x0 - self.r:
-                return True
-
-        if np.hypot(self.x0 - rect.x0, self.y0 - rect.y0) < self.r:
-            return True
-
-        if np.hypot(self.x0 - (rect.x0 + rect.dx), self.y0 - rect.y0) < self.r:
-            return True
-
-        if np.hypot(self.x0 - rect.x0, self.y0 - (rect.y0 + rect.dy)) < self.r:
-            return True
-
-        if np.hypot(self.x0 - (rect.x0 + rect.dx), self.y0 - (rect.y0 + rect.dy)) < self.r:
-            return True
-
-        return False
+        return c1 or c2 or c3 or c4
 
 
 class Rectangle(object):
 
-    def __init__(self, x0, y0, dx, dy, color=(1, 1, 1, 1)):
+    def __init__(self, x0, y0, dx, dy, color=(1, 1, 1, 1), side_bounce=False):
 
-        # Store position and size
-        self.x0 = x0
-        self.y0 = y0
+        self.xcen = x0 + dx / 2.
+        self.ycen = y0 + dy / 2.
         self.dx = dx
         self.dy = dy
+
+        self.side_bounce = side_bounce
 
         # Generate sprite
         pixels = np.array(color) * np.ones((dy, dx, 4)) * 255
         self.sprite = pixels2sprite(pixels)
 
+    @property
+    def xmin(self):
+        return self.xcen - self.dx / 2.
+
+    @property
+    def xmax(self):
+        return self.xcen + self.dx / 2.
+
+    @property
+    def ymin(self):
+        return self.ycen - self.dy / 2.
+
+    @property
+    def ymax(self):
+        return self.ycen + self.dy / 2.
+
     def blit(self):
-        self.sprite.blit(self.x0, self.y0)
+        self.sprite.blit(self.xmin, self.ymin)
 
 r = Rectangle(0, 0, 10, 10, color=(1, 0, 1, 1))
 
@@ -127,32 +175,50 @@ for i in range(NX):
 
 # Set up paddle
 
-paddle = Rectangle(WINDOW_WIDTH / 2. - PADDLE_WIDTH / 2., 0, PADDLE_WIDTH, PADDLE_HEIGHT)
+paddle = Rectangle(WINDOW_WIDTH / 2. - PADDLE_WIDTH / 2., 0, PADDLE_WIDTH, PADDLE_HEIGHT, side_bounce=True)
 
 # Set up ball
 
 import random
 ball = Ball(WINDOW_WIDTH / 2., EMPTY_HEIGHT / 2., BALL_RADIUS, random.uniform(-5, 5), 400)
 
+playing = True
+
 
 def update(dt):
 
+    global playing
+
+    if not playing:
+        return
+
     ball.move(dt)
 
-    if ball.collides(paddle):
-        ball.vy = -ball.vy
-        ball.vx = ball.x0 - paddle.x0 - paddle.dx / 2 * 4
+    ball.collides(paddle)
 
     for brick in bricks:
         if ball.collides(brick):
-            ball.vy = -ball.vy
             bricks.remove(brick)
             break
+
+    if ball.collide_horizontal(0):
+        ball.xcen = WINDOW_WIDTH / 2.
+        ball.ycen = EMPTY_HEIGHT / 2.
+        playing = False
 
 
 @window.event
 def on_mouse_motion(x, y, dx, dy):
-    paddle.x0 = max(0, min(WINDOW_WIDTH, x - paddle.dx / 2.))
+    paddle.xcen = max(0, min(WINDOW_WIDTH, x))
+
+
+@window.event
+def on_mouse_press(*args):
+    global playing
+    if not playing:
+        playing = True
+        ball.vx = random.uniform(-5, 5)
+        ball.vy = 400
 
 
 @window.event
