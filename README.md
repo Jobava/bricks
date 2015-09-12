422f374e52181455dd72d6d34f0133a612ac2d04
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-09-12 23:27:07 +0200

    Improved how settings are handled, updated README.md

--- README.md
+++ README.md
@@ -4,3 +4,29 @@ About
 Experimental clone of the classic 'brick breaking' games. This is just for fun
 and to experiment with the [pyglet](http://www.pyglet.org) library.
 
+![screenshot](screenshot.png)
+
+**Note:** This is not feature complete in any way!
+
+Dependencies
+============
+
+This requires [Numpy](http://www.numpy.org),
+[Matplotlib](http://www.matplotlib.org), and [Pyglet](http://pyglet.org). The latter can easily be installed with:
+
+    pip install pyglet
+
+Running
+=======
+
+To run:
+
+    python bricks.py
+
+If the ball hits the bottom of the screen, just click to start the ball again.
+
+Customizing
+===========
+
+All visual aspects of the game can be edited in ``settings.py`` for now.
+
--- bricks.py
+++ bricks.py
@@ -2,60 +2,55 @@ import random
 
 import pyglet
 import numpy as np
-import matplotlib.pyplot as plt
 
+import settings
 from geometry import HorizontalSegment, VerticalSegment
 from sprites import Ball, Rectangle
 
 # Set global options
 
-EMPTY_HEIGHT = 300
-BRICK_WIDTH = 100
-BRICK_HEIGHT = 18
-NX = 7
-NY = 12
-MARGIN = 5
-PADDLE_WIDTH = 130
-PADDLE_HEIGHT = 17
-BALL_RADIUS = 12
-
-WINDOW_WIDTH = (BRICK_WIDTH + MARGIN) * NX + MARGIN
-WINDOW_HEIGHT = EMPTY_HEIGHT + (BRICK_HEIGHT + MARGIN) * NY
-
-CMAP = plt.cm.jet
-
-# Prepare colormap
-
-COLORS = [plt.cm.jet(x) for x in np.linspace(0., 1, NY)]
-
-WINDOW_EDGES = [HorizontalSegment(0, WINDOW_WIDTH, WINDOW_HEIGHT),
-                VerticalSegment(0, 0, WINDOW_HEIGHT),
-                VerticalSegment(WINDOW_WIDTH, 0, WINDOW_HEIGHT)]
-
-BOTTOM_EDGE = [HorizontalSegment(0, WINDOW_WIDTH, 0)]
+window_width = (settings.brick_width + settings.margin) * settings.nx + settings.margin
+window_height = settings.empty_height + (settings.brick_height + settings.margin) * settings.ny
 
 # Initialize window
 
-window = pyglet.window.Window(width=WINDOW_WIDTH, height=WINDOW_HEIGHT, resizable=False)
+window = pyglet.window.Window(width=window_width, height=window_height, resizable=False)
 window.clear()
+
+# Prepare colormap
+
+colors = [settings.cmap(x) for x in np.linspace(0., 1, settings.ny)]
+
+window_edges = [HorizontalSegment(0, window.width, window.height),
+                VerticalSegment(0, 0, window.height),
+                VerticalSegment(window.width, 0, window.height)]
+
+bottom_edge = [HorizontalSegment(0, window.width, 0)]
+
 
 # Set up bricks
 
 bricks = []
-for i in range(NX):
-    for j in range(NY):
-        x0 = MARGIN + (BRICK_WIDTH + MARGIN) * i
-        y0 = WINDOW_HEIGHT - ((BRICK_HEIGHT + MARGIN) * (j + 1))
-        bricks.append(Rectangle(x0, y0, BRICK_WIDTH, BRICK_HEIGHT, color=COLORS[j]))
-
+for i in range(settings.nx):
+    for j in range(settings.ny):
+        x0 = settings.margin + (settings.brick_width + settings.margin) * i
+        y0 = window.height - ((settings.brick_height + settings.margin) * (j + 1))
+        bricks.append(Rectangle(x0, y0, settings.brick_width, settings.brick_height, color=colors[j]))
 
 # Set up paddle
 
-paddle = Rectangle(WINDOW_WIDTH / 2. - PADDLE_WIDTH / 2., 0, PADDLE_WIDTH, PADDLE_HEIGHT, side_bounce=True)
+paddle = Rectangle(window.width / 2. - settings.paddle_width / 2.,
+                  0,
+                  settings.paddle_width,
+                  settings.paddle_height,
+                  side_bounce=True)
 
 # Set up ball
 
-ball = Ball(WINDOW_WIDTH / 2., EMPTY_HEIGHT / 2., BALL_RADIUS, random.uniform(-5, 5), 400)
+ball = Ball(window.width / 2.,
+            settings.empty_height / 2.,
+            settings.ball_radius,
+            random.uniform(-5, 5), 400)
 
 playing = True
 
@@ -69,7 +64,7 @@ def update(dt):
 
     ball.move(dt)
 
-    segment = ball.check_collision(WINDOW_EDGES)
+    segment = ball.check_collision(window_edges)
 
     side_bounce = False
 
@@ -86,10 +81,10 @@ def update(dt):
                 break
 
     if segment is None:
-        segment = ball.check_collision(BOTTOM_EDGE)
+        segment = ball.check_collision(bottom_edge)
         if segment is not None:
-            ball.xcen = WINDOW_WIDTH / 2.
-            ball.ycen = EMPTY_HEIGHT / 2.
+            ball.xcen = window.width / 2.
+            ball.ycen = settings.empty_height / 2.
             playing = False
     else:
         ball.xcen, ball.ycen, ball.vx, ball.vy = segment.reflect(ball.xcen, ball.ycen,
@@ -99,7 +94,7 @@ def update(dt):
 
 @window.event
 def on_mouse_motion(x, y, dx, dy):
-    paddle.xcen = max(0, min(WINDOW_WIDTH, x))
+    paddle.xcen = max(0, min(window.width, x))
 
 
 @window.event
